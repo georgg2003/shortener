6fe615e564a6b54103e2544074d6d6db8910e3a5
Егор Ткач <egor.tkach@vk.team>
2025-10-06 20:05:33 +0300

    lint

--- internal/usecase/shorten.go
+++ internal/usecase/shorten.go
@@ -12,6 +12,6 @@ const serviceURL = "http://localhost:8080"
 func (s useCase) NewShortURL(url string) string {
 	shortID := utils.RandomBase62(shortIDLength)
 	s.repository.NewShortURL(url, shortID)
-	shortUrl := fmt.Sprintf("%v/%v", serviceURL, shortID)
-	return shortUrl
+	shortURL := fmt.Sprintf("%v/%v", serviceURL, shortID)
+	return shortURL
 }
