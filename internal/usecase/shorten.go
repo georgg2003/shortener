ac5a5fc6154eb32a04dbc0d28432dd58df10face
Егор Ткач <egor.tkach@vk.team>
2025-10-06 20:03:58 +0300

    codestyle

--- internal/usecase/shorten.go
+++ internal/usecase/shorten.go
@@ -1,25 +1,16 @@
 package usecase
 
 import (
-	"crypto/rand"
 	"fmt"
+
+	"github.com/georgg2003/shortener/pkg/utils"
 )
 
-const alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const shortUrlIDLength = 7
+const shortIDLength = 7
 const serviceURL = "http://localhost:8080"
 
-func randomBase62(n int) string {
-	bytes := make([]byte, n)
-	rand.Read(bytes)
-	for i, b := range bytes {
-		bytes[i] = alphabet[int(b)%len(alphabet)]
-	}
-	return string(bytes)
-}
-
 func (s useCase) NewShortURL(url string) string {
-	shortID := randomBase62(shortUrlIDLength)
+	shortID := utils.RandomBase62(shortIDLength)
 	s.repository.NewShortURL(url, shortID)
 	shortUrl := fmt.Sprintf("%v/%v", serviceURL, shortID)
 	return shortUrl
